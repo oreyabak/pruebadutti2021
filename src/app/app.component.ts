69a7fd0c949c401d03e3055af95bdca644ad999a
Jorge Caballero Barranco <oreyabak@gmail.com>
2021-05-13 21:06:23 +0200

    Añadir OnInit al app.componente para controlar si ya tiene sesión abierta.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './auth/services/token-storage.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'massimo-dutti';
+export class AppComponent implements OnInit {
+	title = 'massimo-dutti';
+
+	isLoggedIn = false;
+	username?: string;
+
+	constructor(private tokenStoreageService: TokenStorageService) {}
+
+	ngOnInit(): void {
+		this.isLoggedIn = !!this.tokenStoreageService.getUser();
+
+		if (this.isLoggedIn) {
+			const user = this.tokenStoreageService.getUser();
+			this.username = user.username;
+		}
+	}
+
+	logout(): void {
+		this.tokenStoreageService.signOut();
+		window.location.reload();
+	}
 }
