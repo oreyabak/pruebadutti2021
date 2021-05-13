85a6dd059f0709d107cf878fb437f2cdaaa719dc
Jorge Caballero Barranco <oreyabak@gmail.com>
2021-05-13 22:10:35 +0200

    Loginarse

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-
-// JSON
-import usersList from 'src/assets/json/users.json';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { TokenStorageService } from '../../auth/services/token-storage.service';
 
 @Component({
 	selector: 'app-login',
@@ -14,34 +13,59 @@ export class LoginComponent implements OnInit {
 
 	loginForm: FormGroup;
 	dataLoading: boolean = false;
-	users: any = usersList;
 	unregistered: boolean = false;
 	invalid: boolean = false;
+	isLoggedIn = false;
 
 	constructor(
 		private fb: FormBuilder,
 		private router: Router,
+		private authService: AuthService,
+		private tokenStorage: TokenStorageService,
 	) { }
 
 	ngOnInit(): void {
 		this.loginForm = this.fb.group({
 			username: ['', [Validators.required, Validators.minLength(3)]],
 			password: ['', [Validators.required, Validators.minLength(3)]]
-		})
+		});
+
+		if (this.tokenStorage.getToken()) {
+			this.isLoggedIn = true;
+			this.router.navigate(['/principal/ships']);
+		}
 	}
 
 	// Petición de acceso a la App.
 	loginUser() {
 		if (this.loginForm.invalid) { return }
+
+		this.dataLoading = true;
+		let username = this.loginForm.value.username;
+		let password = this.loginForm.value.password;
+		this.authService.login(username, password).subscribe(
+			data => {
+				this.tokenStorage.saveToken(data.accessToken);
+				this.tokenStorage.saveUser(data);
+
+				this.unregistered = false;
+				this.isLoggedIn = true;
+				this.dataLoading = false;
+				this.reloadPage();
+			},
+			err => {
+				this.unregistered = true;
+				this.dataLoading = false;
+			}
+		);
+
 		// TODO : Falta integrar el servicio para autentificar al usuario
-		// JSON simulando usuarios
-		var userLogin = this.loginForm.value.username;
-		var filterJson = this.users.filter(function (user) { return user.first_name === userLogin });
-		if (filterJson.length > 0) {
-			this.router.navigate(['/principal/ships'])
-		} else {
-			this.unregistered = true;
-		}
+		//var filterJson = this.users.filter(function (user) { return user.first_name === userLogin });
+		//if (filterJson.length > 0) {	this.router.navigate(['/principal/ships'])} else {this.unregistered = true;}
+	}
+
+	reloadPage(): void {
+		window.location.reload();
 	}
 }
 
