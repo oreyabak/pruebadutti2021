1330af91b235e5a534834693300c19eec9dd2492
Jorge Caballero Barranco <oreyabak@gmail.com>
2021-05-13 21:52:34 +0200

    Limpiar código inecesario.

--- src/app/auth/services/user.service.ts
+++ src/app/auth/services/user.service.ts
@@ -11,19 +11,7 @@ export class UserService {
 
 	constructor(private http: HttpClient) { }
 
-	getPublicContent(): Observable<any> {
-		return this.http.get(API_URL + 'all', { responseType: 'text' });
-	}
-
 	getUserBoard(): Observable<any> {
 		return this.http.get(API_URL + 'user', { responseType: 'text' });
-	}
-
-	getModeratorBoard(): Observable<any> {
-		return this.http.get(API_URL + 'mod', { responseType: 'text' });
-	}
-
-	getAdminBoard(): Observable<any> {
-		return this.http.get(API_URL + 'admin', { responseType: 'text' });
 	}
 }
