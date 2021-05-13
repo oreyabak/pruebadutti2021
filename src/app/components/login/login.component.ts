8c2781fe1bdc23ab410d507bf003dec2ee5313e5
Jorge Caballero Barranco <oreyabak@gmail.com>
2021-05-13 21:08:05 +0200

    Primeras modificaciones del actual componente de login para adapterlo al Auth.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,40 +6,42 @@ import { Router } from '@angular/router';
 import usersList from 'src/assets/json/users.json';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-  dataLoading: boolean = false;
-  users: any = usersList;
-  unregistered: boolean = false;
-  invalid: boolean = false;
+	loginForm: FormGroup;
+	dataLoading: boolean = false;
+	users: any = usersList;
+	unregistered: boolean = false;
+	invalid: boolean = false;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router
-  ) { }
+	constructor(
+		private fb: FormBuilder,
+		private router: Router,
+	) { }
 
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: [ '', [Validators.required, Validators.minLength(3)]],
-      password: [ '', [Validators.required, Validators.minLength(6)]]
-    })
-  }
-  loginUser() {
-    if (this.loginForm.invalid) { return }
-    // TODO : Falta integrar el servicio para autentificar al usuario
-    // JSON simulando usuarios
-    var userLogin = this.loginForm.value.username;
-    var filterJson = this.users.filter(function (user) { return user.first_name === userLogin  });
-    if (filterJson.length > 0) {
-      this.router.navigate(['/principal/ships'])
-    } else {
-      this.unregistered = true;
-    }
-  }
+	ngOnInit(): void {
+		this.loginForm = this.fb.group({
+			username: ['', [Validators.required, Validators.minLength(3)]],
+			password: ['', [Validators.required, Validators.minLength(3)]]
+		})
+	}
+
+	// Petición de acceso a la App.
+	loginUser() {
+		if (this.loginForm.invalid) { return }
+		// TODO : Falta integrar el servicio para autentificar al usuario
+		// JSON simulando usuarios
+		var userLogin = this.loginForm.value.username;
+		var filterJson = this.users.filter(function (user) { return user.first_name === userLogin });
+		if (filterJson.length > 0) {
+			this.router.navigate(['/principal/ships'])
+		} else {
+			this.unregistered = true;
+		}
+	}
 }
 
